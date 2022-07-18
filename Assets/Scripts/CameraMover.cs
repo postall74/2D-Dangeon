89498c4942b3f1568fd56b8f1136274744a56a30
postall74 <postall_mcs@mail.ru>
2022-07-19 02:47:49 +0500

    Added Camera script
    Добален скрипт камеры следящей за игроком

--- Assets/Scripts/CameraMover.cs
+++ Assets/Scripts/CameraMover.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class CameraMover : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform _player;
+    [SerializeField] private float _freeHorizontalMovment = 0.15f;
+    [SerializeField] private float _freeVerticalMovment = 0.05f;
+
+
+    private void LateUpdate()
     {
-        
+        Vector3 delta = Vector3.zero;
+
+        delta.x = GetDelta(_player.position.x, transform.position.x, _freeHorizontalMovment);
+        delta.y = GetDelta(_player.position.y, transform.position.y, _freeVerticalMovment);
+
+        transform.position += new Vector3(delta.x, delta.y, 0);
     }
 
-    // Update is called once per frame
-    void Update()
+    private float GetDelta(float playerPositinAxis, float cameraPositionAxis, float size)
     {
-        
+        float deltaAxis = playerPositinAxis - cameraPositionAxis;
+
+        if (deltaAxis > size || deltaAxis < -size)
+        {
+            if (cameraPositionAxis < playerPositinAxis)
+                return deltaAxis - size;
+            else
+                return deltaAxis + size;
+        }
+        return deltaAxis;
     }
 }
