e89e2f7f0230cb7039ff61b06c2446c46fe1e456
postall74 <postall_mcs@mail.ru>
2022-07-19 01:57:34 +0500

    Added collision
    Добавлена возможность сталкиваться с предметами на разных слоях (Layer)

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,9 +6,12 @@ public class Player : MonoBehaviour
 {
     private const string Horizontal = "Horizontal";
     private const string Vertical = "Vertical";
+    private const string Actors = "Actors";
+    private const string Blocking = "Blocking";
 
     private BoxCollider2D _collider;
     private Vector3 _moveDelta;
+    private RaycastHit2D _hit;
 
 
     private void Start()
@@ -28,6 +31,14 @@ public class Player : MonoBehaviour
         else if (_moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
-        transform.Translate(_moveDelta * Time.deltaTime);
+        _hit = Physics2D.BoxCast(transform.position, _collider.size, 0, new Vector2(0, _moveDelta.y), Mathf.Abs(_moveDelta.y * Time.deltaTime), LayerMask.GetMask(Actors, Blocking));
+
+        if (_hit.collider == null)
+            transform.Translate(0, _moveDelta.y * Time.deltaTime, 0);
+
+        _hit = Physics2D.BoxCast(transform.position, _collider.size, 0, new Vector2(_moveDelta.x, 0), Mathf.Abs(_moveDelta.x * Time.deltaTime), LayerMask.GetMask(Actors, Blocking));
+
+        if (_hit.collider == null)
+            transform.Translate(_moveDelta.x * Time.deltaTime, 0, 0);
     }
 }
