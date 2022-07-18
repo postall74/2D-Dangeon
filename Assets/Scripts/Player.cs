b489ce7b3ff25161adda197470598fae969bed73
postall74 <postall_mcs@mail.ru>
2022-07-19 01:05:36 +0500

    Mover
    Добавлена хотьба по  оси Horizontal и поворот в сторону хотьбы

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,33 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(BoxCollider2D))]
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private const string Horizontal = "Horizontal";
+    private const string Vertical = "Vertical";
+
+    private BoxCollider2D _collider;
+    private Vector3 _moveDelta;
+
+
+    private void Start()
     {
-        
+        _collider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        float horizontal = Input.GetAxisRaw(Horizontal);
+        float vertical = Input.GetAxisRaw(Vertical);
+
+        _moveDelta = new Vector3(horizontal, vertical, 0);
+
+        if (_moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (_moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        transform.Translate(_moveDelta * Time.deltaTime);
     }
 }
